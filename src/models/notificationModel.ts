194a141ad2ec0758fe4e4c644368b608ebf698db
maherali22 <mahiralimattummal@gmail.com>
2025-04-06 18:23:35 +0530

    feat: add course management models and routes, including course creation and notifications

---
+++ src/models/notificationModel.ts
@@ -0,0 +1,31 @@
+import mongoose, {Document, Model , Schema} from "mongoose";
+
+export interface INotification extends Document {
+  userId: string;
+  title: string;
+  message: string;
+  status: string;
+}
+
+const notificationSchema = new Schema<INotification>({
+  title: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    default: "unread",
+    required: true,
+  },
+
+}, {
+  timestamps: true,
+});
+
+const NotificationModel: Model<INotification> = mongoose.model("Notification", notificationSchema);
+
+export default NotificationModel; 
